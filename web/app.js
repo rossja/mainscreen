be149bd02ce5d335a350d3bb865a4ca7c279c944
rossja <algorythm@gmail.com>
2015-03-26 06:59:01 -0400

    added twitter stream basics

--- web/app.js
+++ web/app.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var app = express();
+var secrets = require('./secrets');
+
+var trackwords = 'BSidesROC BSIDESROC bsidesroc bsidesROC'
 
 app.get('/', function(request, response) {
   response.send('OK');
 });
 
+get_stream(trackwords)
+
+function get_stream(trackwords) {
+  var Stream = require('user-stream');
+  var stream = new Stream({
+    consumer_key: secrets.consumer_key,
+    consumer_secret: secrets.consumer_secret,
+    access_token_key: secrets.access_token_key,
+    access_token_secret: secrets.access_token_secret
+  });
+  var params = {
+    follow: q
+  }
+
+  //create stream
+  stream.stream(params);
+
+  //listen stream data
+  stream.on('data', function(json) {
+   console.log(json);
+  });
+}
 module.exports = app;
---
+++ web/secrets.js
@@ -0,0 +1,6 @@
+var consumer_key, consumer_secret, access_token_key, access_token_secret;
+
+consumer_key = 'your consumer key';
+consumer_secret = 'your consumer secret';
+access_token_key = 'your access token key';
+access_token_secret = 'your access token secret';
