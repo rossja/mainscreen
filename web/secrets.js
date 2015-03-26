d7efad3d07b92145fddfdddc3a86b988a3005c64
rossja <algorythm@gmail.com>
2015-03-26 07:00:55 -0400

    bleh

--- web/secrets.js
+++
@@ -1,6 +0,0 @@
-var consumer_key, consumer_secret, access_token_key, access_token_secret;
-
-consumer_key = 'GuS8fSTPlMQpiYdMYkFYhZQcz';
-consumer_secret = 'gFK1rRGHjZiBeIywXimUwQZWOpecYavDIsod4ixIGve20qzyyB';
-access_token_key = '218677519-9AQ8FYU0NZrdrXi9K9AjvgVokf5lHP4lnvQYtb0r';
-access_token_secret = 'xmhNdq42qHjtSqEkOTswp8kGUoNORMVY2I0UTvLNcSwNH';
