e29e8be439fd58a09de2805eba0bde29b7a1a4c8
rossja <algorythm@gmail.com>
2015-03-26 07:00:09 -0400

    added secrets to gitignore

--- web/secrets.js
+++ web/secrets.js
@@ -1,6 +1,6 @@
 var consumer_key, consumer_secret, access_token_key, access_token_secret;
 
-consumer_key = 'your consumer key';
-consumer_secret = 'your consumer secret';
-access_token_key = 'your access token key';
-access_token_secret = 'your access token secret';
+consumer_key = 'GuS8fSTPlMQpiYdMYkFYhZQcz';
+consumer_secret = 'gFK1rRGHjZiBeIywXimUwQZWOpecYavDIsod4ixIGve20qzyyB';
+access_token_key = '218677519-9AQ8FYU0NZrdrXi9K9AjvgVokf5lHP4lnvQYtb0r';
+access_token_secret = 'xmhNdq42qHjtSqEkOTswp8kGUoNORMVY2I0UTvLNcSwNH';
